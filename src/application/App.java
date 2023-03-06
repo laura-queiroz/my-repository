db0adeb60007bd8f55998c2eef3e8bac1c8c7c3d
laura-queiroz <117654629+laura-queiroz@users.noreply.github.com>
2023-03-06 10:35:33 -0300

    [PO-463] Criação entidade cliente (#3)
    * [PO-463] Mensagem de teste
    
    * [PO-463] Novo commit
    
    * [PO-463] Criação entidade cliente

--- src/application/App.java
+++ src/application/App.java
@@ -1,8 +1,12 @@
 package application;
 
+import application.entities.Client;
+
 public class App {
     public static void main (String [] args) {
         System.out.println("Mensagem de teste");
         System.out.println("Novo commit");
+        Client c = new Client(1, "Ana");
+        System.out.print(c);
     }
 }
