19592561137aa73e7f2e7149de7279a508d26b58
laura-queiroz <117654629+laura-queiroz@users.noreply.github.com>
2023-03-06 10:43:24 -0300

    [PO-463] Novo cliente na branch teste (#4)
    * [PO-463] Mensagem de teste
    
    * [PO-463] Novo commit
    
    * [PO-463] Criação entidade cliente
    
    * [PO-463] Novo cliente na branch teste

--- src/application/App.java
+++ src/application/App.java
@@ -6,7 +6,12 @@ public class App {
     public static void main (String [] args) {
         System.out.println("Mensagem de teste");
         System.out.println("Novo commit");
+
         Client c = new Client(1, "Ana");
         System.out.print(c);
+
+        Client c1 = new Client(2, "Ana Clara");
+        System.out.print(c1);
+
     }
 }
