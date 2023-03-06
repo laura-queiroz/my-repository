547636c98c3fba1d6aa38daf19d2f2ebb6e2785a
Laura Sa <laura.sa@youse.com.br>
2023-03-06 11:38:45 -0300

    Nova entidade produto

--- src/application/App.java
+++ src/application/App.java
@@ -1,20 +1,26 @@
 package application;
 
 import application.entities.Client;
+import application.entities.Product;
 
 public class App {
     public static void main (String [] args) {
-        System.out.println("Mensagem de teste");
-        System.out.println("Novo commit");
+        System.out.println("Clientes e Produtos:");
 
         Client c = new Client(1, "Ana");
-        System.out.print(c);
+        System.out.println(c);
 
         Client c1 = new Client(2, "Ana Clara");
-        System.out.print(c1);
+        System.out.println(c1);
 
         Client c2 = new Client(3, "Ana Clarice");
-        System.out.print(c2);
+        System.out.println(c2);
+
+        Product p = new Product(1, "Laptop", 2.159,1);
+        System.out.println(p);
+
+        Product p1 = new Product(2, "Phone", 1.195,2);
+        System.out.println(p1);
 
     }
 }
