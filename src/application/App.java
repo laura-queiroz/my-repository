710b2ad193b3b78cf1488680f279dbcfca21f592
laura-queiroz <117654629+laura-queiroz@users.noreply.github.com>
2023-03-06 10:43:24 -0300

    [PO-463] Novos clientes na branch teste

--- src/application/App.java
+++ src/application/App.java
@@ -6,7 +6,15 @@ public class App {
     public static void main (String [] args) {
         System.out.println("Mensagem de teste");
         System.out.println("Novo commit");
+
         Client c = new Client(1, "Ana");
         System.out.print(c);
+
+        Client c1 = new Client(2, "Ana Clara");
+        System.out.print(c1);
+
+        Client c2 = new Client(3, "Ana Clarice");
+        System.out.print(c2);
+
     }
 }
