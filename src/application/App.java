39bb6d7042c25523c88c18d0e0e5424340d96c24
Laura Sa <laura.sa@youse.com.br>
2023-03-06 10:12:35 -0300

    [PO-463] Mensagem de teste

--- src/application/App.java
+++ src/application/App.java
@@ -1,5 +1,7 @@
 package application;
 
 public class App {
-    public static void main (String [] args) {}
+    public static void main (String [] args) {
+        System.out.println("Mensagem de teste");
+    }
 }
